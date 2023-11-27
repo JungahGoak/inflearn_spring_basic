7cff15205170221157711099f9a332b687a81e47
JungAh <70677614+Jungahgo@users.noreply.github.com>
2023-11-27 22:42:15 +0900

    section2) 회원 도메인 실행과 테스트 - DIP 위반 문제

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    // 실제 할당하는 new MemoryMemberRepository가 구현체를 의존하는 문제
+    // 추상화 & 구체화 모두 의존 => DIP 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     @Override
     public void join(Member member) {
