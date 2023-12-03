e1114febd3904e6a164ec27cfaafe9b37de569be
JungAh <70677614+Jungahgo@users.noreply.github.com>
2023-12-03 14:05:04 +0900

    section5) 싱글톤 방식의 주의점 - stateful인경우

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -38,6 +40,29 @@ public class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+
+        //AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        //1. 조회: 호출헐 때마다 객체 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2. 조회: 호출할 때마다 객체 생성
+
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+        //참조값이 다른 것 확인
+        //웹어플리케이션 특성상 동시 요청이 많음
+        //jvm 메모리에 계속 다른 객체가 생성되는 것은 비효율적임 => 싱글톤 패턴으로 해결
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 != memberService2
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+
     }
 
 }
