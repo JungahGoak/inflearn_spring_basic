4107e95bbd371d0f107c4eb8b93cdaef9e683670
JungAh <70677614+Jungahgo@users.noreply.github.com>
2023-12-04 12:27:29 +0900

    section7) 다양한 의조관계 주입방법 - 1. 생성자 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -16,10 +16,10 @@ public class OrderServiceImpl implements OrderService {
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
     // 해결방안: 클라이언트의 OrderServiceImpl에 DiscountPolicy와 구현객체를 대신 생성하고 주입해주어야함
-    private DiscountPolicy discountPolicy;
-    private MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
 
-    //생성자 주입
+    //생성자 주입: 생성자 호출시점에서 딱 1번만 호출되는 것이 보장, 불변 필수 의존관계에서 사용
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.discountPolicy = discountPolicy;
