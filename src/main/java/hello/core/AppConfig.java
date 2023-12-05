3041c9134bf332599a97ec6c87d0f4e316121335
JungAh <70677614+Jungahgo@users.noreply.github.com>
2023-12-05 15:46:22 +0900

    section7) 옵션 처리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -11,6 +11,7 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 // 객체의 생성과 연결 담당
 // AppConfig는 애플리케이션  실제 동작에 필요한 구현 객체를 생성
@@ -20,23 +21,28 @@ public class AppConfig {
 
     // AppConfig는 생성한 객체 인스턴스의 참조를 생성자를 통해서 주입
     // => 생성자 주입
+    @Primary
     @Bean
     public MemberService memberService() {
         
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Primary
     @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+
+    @Primary
     @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), getDiscountPolicy());
         //return null;
     }
 
+    @Primary
     @Bean
     private static DiscountPolicy getDiscountPolicy() {
         return new RateDiscountPolicy();
