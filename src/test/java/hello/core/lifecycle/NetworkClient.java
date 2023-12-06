b21e27417353920b4bcf4abcea92c30e4c338111
JungAh <70677614+Jungahgo@users.noreply.github.com>
2023-12-06 18:59:07 +0900

    section8) 빈 생명주기 콜백 시작
    - 3가지 방법으로 콜백 지원
    1. 인터페이스
    2. 설정 정보에 초기화 매서드, 종료 매서드 지정
    3. @PostConstruct, @PreDestory 애노테이션 지원

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,39 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+    /*
+    데이터베이스 커넥션 풀이나 네트워크 소켓처럼 애플리케이션 시작 시점에
+    필요한 연결을 미리 해두고, 애플리케이션 종료 시점에 연결을 모두 종료하려면,
+    객체 초기화 종료 작업 필요 -> 스프링을 통해 어떻게 진행하는지
+    */
+
+    //spring bean:
+    // 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 ->
+    // 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("url = " + url);
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect(){
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call = " + url + ", message = " + message);
+    }
+
+    //서비스 종료시 호출
+    public void disconnect(){
+        System.out.println("close");
+    }
+}
