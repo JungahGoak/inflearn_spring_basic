13bc812ee1288c82450981722feb43a17bd951e7
JungAh <70677614+Jungahgo@users.noreply.github.com>
2023-12-08 20:31:51 +0900

    section9) request 스코프 예제 만들기

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,37 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.service.annotation.PostExchange;
+
+import java.util.UUID;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    //동시에 여러 HTTP 요청이 오면 어떤 요청인지 알기 어려움 -> request 스코프 사용
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message){
+        System.out.println("message = " + message + "uuid = " + uuid);
+    }
+
+    @PostConstruct
+    public void init(){
+        uuid = UUID.randomUUID().toString();
+        System.out.println("init uuid = " + uuid + this);
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("close uuid = " + uuid + this);
+    }
+}
