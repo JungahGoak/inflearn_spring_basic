f173f244f46ebd6d11ebd71a62057dee81f6c4a8
JungAh <70677614+Jungahgo@users.noreply.github.com>
2023-12-09 00:31:30 +0900

    section9) 스코프와 프록시

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -3,13 +3,14 @@ package hello.core.common;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.service.annotation.PostExchange;
 
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
 
     //동시에 여러 HTTP 요청이 오면 어떤 요청인지 알기 어려움 -> request 스코프 사용
